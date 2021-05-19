de3d2d77ca7c507dcba29f4c0f8a89542ad1b9cb
ccj-cxy <994173486@qq.com>
2021-05-19 16:14:20 +0800

    补全

---
+++ user-provider/src/main/java/com/snk/userprovider/dao/WaybillMapper.java
@@ -0,0 +1,18 @@
+package com.snk.userprovider.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.snk.userprovider.pojo.domain.WaybillDO;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 运单 Mapper 接口
+ * </p>
+ *
+ * @author Cai.ChangJun
+ * @since 2021-05-14
+ */
+@Mapper
+public interface WaybillMapper extends BaseMapper<WaybillDO> {
+
+}
