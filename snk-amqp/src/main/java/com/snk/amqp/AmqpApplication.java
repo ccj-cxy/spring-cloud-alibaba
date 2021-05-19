967b5786ae55c14df98a8b60c1cfeb6a8a3a4ea9
ccj-cxy <994173486@qq.com>
2021-05-19 23:05:34 +0800

    开启openfeign

--- snk-amqp/src/main/java/com/snk/amqp/AmqpApplication.java
+++ snk-amqp/src/main/java/com/snk/amqp/AmqpApplication.java
@@ -2,6 +2,11 @@ package com.snk.amqp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author :Cai.ChangJun
@@ -10,9 +15,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date : 2021/5/7
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class AmqpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AmqpApplication.class);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
