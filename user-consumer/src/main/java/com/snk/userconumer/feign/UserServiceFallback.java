416cb8c44487d25c13d6af16f296d0df7a61dc4e
ccj-cxy <994173486@qq.com>
2021-05-21 15:47:10 +0800

    feign接口回调修改

--- user-consumer/src/main/java/com/snk/userconumer/feign/UserServiceFallback.java
+++ user-consumer/src/main/java/com/snk/userconumer/feign/UserServiceFallback.java
@@ -1,11 +1,20 @@
 package com.snk.userconumer.feign;
 
+import com.snk.common.exception.BusinessException;
+import feign.hystrix.FallbackFactory;
 import org.springframework.stereotype.Service;
 
+/**
+ *
+ * @author Cai.ChangJun
+ * @version 1.0.0
+ * @Date 2021/5/21 15:28
+ */
 @Service
-public class UserServiceFallback implements UserService {
+public class UserServiceFallback implements FallbackFactory<UserService> {
+
     @Override
-    public String config() {
-        return "user-fallback";
+    public UserService create(Throwable throwable) {
+        throw new BusinessException("远程调用运单服务失败，消息无法投递");
     }
 }
