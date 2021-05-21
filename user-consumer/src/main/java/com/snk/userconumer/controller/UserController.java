91a96e717a4330211d530ab66ee9c5df23fae3d7
ccj-cxy <994173486@qq.com>
2021-05-21 14:59:56 +0800

    添加swagger注释

--- user-consumer/src/main/java/com/snk/userconumer/controller/UserController.java
+++ user-consumer/src/main/java/com/snk/userconumer/controller/UserController.java
@@ -2,6 +2,8 @@ package com.snk.userconumer.controller;
 
 import com.snk.common.exception.BusinessException;
 import com.snk.userconumer.feign.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
@@ -10,17 +12,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * openfeign演化测试类
+ * @author Cai.ChangJun
+ * @version 1.0.0
+ * @Date 2021/5/14 22:49
+ */
 @RestController
 @RequestMapping("user")
+@Api(value = "微服务测试接口",tags = "openfeign衍化过程")
 public class UserController {
     @Autowired
     private LoadBalancerClient loadBalancerClient;
 
     /***/
-    @RequestMapping("consumer")
+    @GetMapping("consumer")
+    @ApiOperation(value = "远程调用",notes = "通过restTemplate根据ip+port发送请求远程调用")
     public String consumer() {
         ServiceInstance provider = loadBalancerClient.choose("user-provider");
-        String url = "http://"+provider.getHost()+":"+provider.getPort()+"/user/com.snk.email.config";
+        String url = "http://"+provider.getHost()+":"+provider.getPort()+"/user/config";
         RestTemplate restTemp = new RestTemplate();
         String result = restTemp.getForObject(url, String.class);
         return result;
@@ -30,7 +40,8 @@ public class UserController {
     /**feign*/
     @Autowired
     private UserService userService;
-    @RequestMapping("consumer-feign")
+    @GetMapping("consumer-feign")
+    @ApiOperation(value = "远程调用",notes = "通过feign发送请求远程调用")
     public String userService() {
         String result = userService.config();
         return result;
@@ -39,7 +50,8 @@ public class UserController {
     /**Ribbon*/
     @Autowired
     private RestTemplate restTemplate;
-    @RequestMapping("consumer-ribbon")
+    @GetMapping("consumer-ribbon")
+    @ApiOperation(value = "远程调用",notes = "通过ribbon发送请求远程调用")
     public String consumerribbon() {
         String url = "http://user-provider/user/config";
         String result = restTemplate.getForObject(url, String.class);
