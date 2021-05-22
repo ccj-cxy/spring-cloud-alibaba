3b2e368deda20d20a652728f163ad4e8a000396d
ccj-cxy <994173486@qq.com>
2021-05-22 17:34:50 +0800

    回调修改

--- user-consumer/src/main/java/com/snk/userconumer/feign/UserService.java
+++ user-consumer/src/main/java/com/snk/userconumer/feign/UserService.java
@@ -3,7 +3,7 @@ package com.snk.userconumer.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "user-provider",fallback = UserServiceFallback.class)
+@FeignClient(name = "user-provider",fallbackFactory = UserServiceFallback.class)
 public interface UserService {
     @RequestMapping("/user/config")
     String config();
