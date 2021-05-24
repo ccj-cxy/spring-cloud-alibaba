62a6b7f71cdf7beed9a26ec536c03a234194835d
ccj-cxy <994173486@qq.com>
2021-05-24 15:26:10 +0800

    添加feign测试

---
+++ snk-amqp/src/test/java/com/snk/amqp/FeignTest.java
@@ -0,0 +1,25 @@
+package com.snk.amqp;
+
+import com.snk.amqp.feign.UserConSumerService;
+import com.snk.amqp.pojo.domain.BrokerMessageLogDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author :Cai.ChangJun
+ * @version 1.0.0
+ * @Description :
+ * @Date : 2021/5/18
+ */
+@SpringBootTest
+public class FeignTest {
+
+    @Autowired
+    UserConSumerService userConSumerService;
+
+    @Test
+    void test() {
+        Boolean aBoolean = userConSumerService.modifyMessageStatus(new BrokerMessageLogDTO().setMessageId("149732447200874496").setStatus("1"));
+    }
+}
