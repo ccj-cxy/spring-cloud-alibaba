cb4eb3d82fdccaf1f0254d98a6082782a01c68fa
ccj-cxy <994173486@qq.com>
2021-05-25 16:27:28 +0800

    修改controller返回值为String时报错

--- snk-web-common/snk-common-core/src/main/java/com/snk/common/webRetrun/WebConfiguration.java
+++ snk-web-common/snk-common-core/src/main/java/com/snk/common/webRetrun/WebConfiguration.java
@@ -2,7 +2,7 @@ package com.snk.common.webRetrun;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,6 +18,9 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(0, new MappingJackson2HttpMessageConverter());
+        // 第一种方式是将 json 处理的转换器放到第一位并且是继承GenericHttpMessageConverter，这样json转换器就会进行处理 String转换器就处理不了了。
+        //converters.add(0, new MappingJackson2HttpMessageConverter());
+        // 第二种就是把String类型的转换器去掉，会找到后面的转换器进行转换
+        converters.removeIf(httpMessageConverter -> httpMessageConverter.getClass() == StringHttpMessageConverter.class);
     }
 }
