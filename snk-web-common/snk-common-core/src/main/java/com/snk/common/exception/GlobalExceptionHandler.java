191b2713fc65b9d52bc81e72c77910da9ee56be2
ccj-cxy <994173486@qq.com>
2021-05-26 12:35:09 +0800

    添加参数验证异常拦截

--- snk-web-common/snk-common-core/src/main/java/com/snk/common/exception/GlobalExceptionHandler.java
+++ snk-web-common/snk-common-core/src/main/java/com/snk/common/exception/GlobalExceptionHandler.java
@@ -2,13 +2,20 @@ package com.snk.common.exception;
 
 import com.snk.common.webRetrun.ErrorCode;
 import com.snk.common.webRetrun.Response;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author :Cai.ChangJun
@@ -58,6 +65,25 @@ public class GlobalExceptionHandler {
     {
         log.error(e.getMessage(), e);
         return new Response(new ErrorCode(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Response exceptionHandler(MethodArgumentNotValidException exception){
+        BindingResult result = exception.getBindingResult();
+        StringBuilder stringBuilder = new StringBuilder();
+        AtomicReference<String> message = new AtomicReference<>("");
+        if (result.hasErrors()) {
+            List<ObjectError> errors = result.getAllErrors();
+            if (errors != null) {
+                errors.forEach(p -> {
+                    FieldError fieldError = (FieldError) p;
+                    message.set(fieldError.getDefaultMessage());
+                    log.warn("有问题的请求参数: dto entity [{}],field [{}],message [{}]",fieldError.getObjectName(), fieldError.getField(), fieldError.getDefaultMessage());
+                    stringBuilder.append(fieldError.getDefaultMessage());
+                });
+            }
+        }
+        return new Response(403, StringUtils.isNotBlank(message.get())?message:exception.getMessage());
     }
 
 }
