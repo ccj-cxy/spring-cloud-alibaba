61c479753102150f51f763308942c592fef3d780
ccj-cxy <994173486@qq.com>
2021-05-27 11:18:44 +0800

    添加jwt缓存redis的常量

---
+++ snk-web-common/snk-common-jwt/src/main/java/com/snk/jwt/constants/TokenConstant.java
@@ -0,0 +1,11 @@
+package com.snk.jwt.constants;
+
+/**
+ * @author :Cai.ChangJun
+ * @version 1.0.0
+ * @Description : token常量
+ * @Date : 2021/5/26
+ */
+public class TokenConstant {
+    public static final String CACHE_TOKEN_KEY = "token:";
+}
