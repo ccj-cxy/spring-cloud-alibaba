c5d4b83f31e50df2ffd5210cc37ee124a6e64d22
ccj-cxy <994173486@qq.com>
2021-06-01 19:34:56 +0800

    添加默认登录类型

--- snk-auth/src/main/java/com/snk/auth/pojo/param/LoginParam.java
+++ snk-auth/src/main/java/com/snk/auth/pojo/param/LoginParam.java
@@ -6,7 +6,6 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 /**
  * @author :Cai.ChangJun
