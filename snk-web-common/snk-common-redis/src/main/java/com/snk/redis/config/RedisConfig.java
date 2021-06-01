2c2c18807890cf34bb6dbf3153ca52c33579e057
ccj-cxy <994173486@qq.com>
2021-06-01 11:20:46 +0800

    对redisCacheManager进行配置 设置默认缓存周期切包装子类如果开启了缓存 对子项目的缓存统一设置过期时间 保证最终缓存一致性

--- snk-web-common/snk-common-redis/src/main/java/com/snk/redis/config/RedisConfig.java
+++ snk-web-common/snk-common-redis/src/main/java/com/snk/redis/config/RedisConfig.java
@@ -7,12 +7,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.snk.redis.utils.RedisUtil;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 /**
  * @author :Cai.ChangJun
@@ -23,6 +29,16 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @AutoConfigureAfter(RedisAutoConfiguration.class)
 public class RedisConfig {
+
+    @SuppressWarnings("rawtypes")
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration=RedisCacheConfiguration.defaultCacheConfig()
+                // 设置缓存有效期一小时;
+                .entryTtl(Duration.ofHours(1));
+        return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+                .cacheDefaults(redisCacheConfiguration).build();
+    }
 
     @Bean
     public StringRedisTemplate redisTemplate(LettuceConnectionFactory redisConnectionFactory){
