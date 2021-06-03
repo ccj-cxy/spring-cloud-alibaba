f43df8b5dd363684330546a29d1084360ed3a559
ccj-cxy <994173486@qq.com>
2021-06-03 14:17:26 +0800

    修改get请求

--- snk-auth/src/main/java/com/snk/auth/controller/PublicResourceController.java
+++ snk-auth/src/main/java/com/snk/auth/controller/PublicResourceController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -58,7 +59,7 @@ public class PublicResourceController {
         return resourceIds.size();
     }
 
-    @DeleteMapping("/tree")
+    @GetMapping("/tree")
     @ApiOperation(value = "获取资源树" ,notes = "获取资源树")
     public List<ResourceTreeDTO> getTree() {
         return publicResourceService.getResourceTree();
