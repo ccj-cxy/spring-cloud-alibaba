2cb418fef76a75ba027d447a4287a96733b691f7
ccj-cxy <994173486@qq.com>
2021-06-06 22:17:38 +0800

    添加组织表

--- cloud-resources/snk-auth/snk-auth.sql
+++ cloud-resources/snk-auth/snk-auth.sql
@@ -11,11 +11,43 @@
  Target Server Version : 80019
  File Encoding         : 65001
 
- Date: 28/05/2021 20:23:29
+ Date: 06/06/2021 22:16:55
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for public_org
+-- ----------------------------
+DROP TABLE IF EXISTS `public_org`;
+CREATE TABLE `public_org`  (
+  `ID` int(0) NOT NULL AUTO_INCREMENT COMMENT '机构ID',
+  `CODE` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '机构代号',
+  `NAME` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '机构名',
+  `FULL_NAME` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '机构路径全称',
+  `SHORT_NAME` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '机构简称',
+  `SORT_CODE` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '排序代码',
+  `PARENT_ID` int(0) NULL DEFAULT NULL COMMENT '上级机构',
+  `LEVEL` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '机构级别',
+  `ORG_TYPE` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '机构类型',
+  `LEADER` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '负责人',
+  `REMARK` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '机构说明',
+  `CREATED_BY` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '创建人',
+  `CREATED_TIME` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `UPDATED_BY` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '更新人',
+  `UPDATED_TIME` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '部门机构 ' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of public_org
+-- ----------------------------
+INSERT INTO `public_org` VALUES (1, '0', 'xxx集团公司', NULL, '总公司', '0', 0, '0', '0', '0', '总公司', '0', '2021-06-06 18:44:23', '0', '2021-06-06 18:44:29');
+INSERT INTO `public_org` VALUES (2, '1', 'xxx有限公司', NULL, '分公司', '0', 1, '1', '1', '1', '分公司', '0', '2021-06-06 18:45:35', '0', '2021-06-06 18:45:42');
+INSERT INTO `public_org` VALUES (3, '2', '开发部', NULL, '开发部门', '1', 2, '2', '2', '1', '职能部门', NULL, NULL, NULL, NULL);
+INSERT INTO `public_org` VALUES (4, '3', '测试部', NULL, '测试部门', '2', 2, '2', '2', '1', '职能部门', '0', '2021-06-06 18:47:40', '0', '2021-06-06 18:47:45');
+INSERT INTO `public_org` VALUES (5, '4', '业务部门', NULL, '业务部门', '3', 2, '2', '2', '1', '职能部门', '0', NULL, '0', NULL);
 
 -- ----------------------------
 -- Table structure for public_resource
@@ -30,7 +62,7 @@ CREATE TABLE `public_resource`  (
   `icon` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '资源图标',
   `parent_id` int(0) NOT NULL DEFAULT 0 COMMENT '父级资源id',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '资源表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '资源表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of public_resource
@@ -51,14 +83,14 @@ CREATE TABLE `public_role`  (
   `start_time` datetime(0) NULL DEFAULT NULL COMMENT '角色生效开始时间',
   `end_time` datetime(0) NULL DEFAULT NULL COMMENT '角色生效结束时间',
   `defaulted` tinyint(1) NOT NULL DEFAULT 0 COMMENT '默认角色 0:非默认角色 1:默认角色',
-  `web_id` int(0) NOT NULL COMMENT '站点id',
+  `org_id` int(0) NOT NULL COMMENT '站点id',
   `create_time` datetime(0) NOT NULL COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `uk_name_web_id`(`name`, `web_id`) USING BTREE,
+  UNIQUE INDEX `uk_name_web_id`(`name`, `org_id`) USING BTREE,
   INDEX `idx_name`(`name`) USING BTREE,
-  INDEX `fk_web_id`(`web_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
+  INDEX `fk_web_id`(`org_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of public_role
@@ -77,7 +109,7 @@ CREATE TABLE `public_role_resource`  (
   INDEX `fk_role_id`(`role_id`) USING BTREE,
   INDEX `fk_resource_id`(`resource_id`) USING BTREE,
   INDEX `uk_role_id_resource_id`(`role_id`, `resource_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色资源关联表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色资源关联表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of public_role_resource
@@ -104,15 +136,15 @@ CREATE TABLE `public_user`  (
   `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '0:系统管理员 1:站点管理员 2:普通用户',
   `create_time` datetime(0) NOT NULL COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  `web_id` int(0) NOT NULL COMMENT '站点id',
+  `org_id` int(0) NOT NULL COMMENT '站点id',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `name`(`name`) USING BTREE COMMENT '唯一索引，用户名必须唯一',
   INDEX `idx_nick_name`(`nike_name`) USING BTREE,
   INDEX `idx_mobile_phone`(`mobile_phone`) USING BTREE,
   INDEX `idx_email`(`email`) USING BTREE,
   INDEX `idx_create_time`(`create_time`) USING BTREE,
-  INDEX `fk_web_id`(`web_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+  INDEX `fk_web_id`(`org_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of public_user
@@ -120,6 +152,8 @@ CREATE TABLE `public_user`  (
 INSERT INTO `public_user` VALUES (1, 'zs', 'MTIz', 'zs', '123', '123', '123', 1, 0, '2021-05-26 22:20:02', '2021-05-26 22:20:02', 0);
 INSERT INTO `public_user` VALUES (2, 'lisi', 'MTIz', 'lisi', '123', '123', '123', 1, 1, '2021-05-26 22:24:39', '2021-05-26 22:24:39', 0);
 INSERT INTO `public_user` VALUES (3, 'wangwu', 'MTIzNDU2', 'wangwu', '123', '123', '123', 1, 1, '2021-05-26 22:31:34', '2021-05-26 22:31:34', 0);
+INSERT INTO `public_user` VALUES (12, 'ccj', 'MTIzNDU2', 'snk', '1111111', '99', 'caic.j@qq.com', 1, 2, '2021-06-02 16:09:35', '2021-06-02 16:09:35', 0);
+INSERT INTO `public_user` VALUES (19, 'ml', 'MTIzNDU2', 'snk', '1111111', '99', 'caic.j@qq.com', 1, 2, '2021-06-02 16:47:10', '2021-06-02 16:47:10', 0);
 
 -- ----------------------------
 -- Table structure for public_user_role
@@ -133,7 +167,7 @@ CREATE TABLE `public_user_role`  (
   UNIQUE INDEX `uk_user_id_role_id`(`user_id`, `role_id`) USING BTREE,
   INDEX `fk_user_id`(`user_id`) USING BTREE,
   INDEX `fk_role_id`(`role_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户角色关联表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户角色关联表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of public_user_role
