47942365ec2489771a58dfe0de638bd482392044
ccj-cxy <994173486@qq.com>
2021-06-07 00:26:17 +0800

    配置网关跨域

---
+++ geteway/src/main/java/com/snk/gateway/config/CrossOriginFilter.java
@@ -0,0 +1,30 @@
+package com.snk.gateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * description: 跨域访问
+ *
+ * @author wangpeng
+ * @date 2018/09/25
+ */
+@Configuration
+public class CrossOriginFilter {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
