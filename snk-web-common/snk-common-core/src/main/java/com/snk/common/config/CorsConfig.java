4eb7a1bba2f3490ed15ea6548c53faf21c580a41
ccj-cxy <994173486@qq.com>
2021-06-07 12:25:08 +0800

    配置跨域信息

--- snk-web-common/snk-common-core/src/main/java/com/snk/common/config/CorsConfig.java
+++ snk-web-common/snk-common-core/src/main/java/com/snk/common/config/CorsConfig.java
@@ -2,8 +2,11 @@ package com.snk.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 /**
  *跨域配置
@@ -17,25 +20,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            //重写父类提供的跨域请求处理的接口
-            public void addCorsMappings(CorsRegistry registry) {
-                //添加映射路径
-                registry.addMapping("/**")
-                        //放行哪些原始域
-                        .allowedOrigins("*")
-                        //是否发送Cookie信息
-                        .allowCredentials(true)
-                        //放行哪些原始域(请求方式)
-                        .allowedMethods("GET","POST", "PUT", "DELETE")
-                        //放行哪些原始域(头部信息)
-                        .allowedHeaders("*")
-                        //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
-                        .exposedHeaders("Header1", "Header2");
-            }
-        };
-
+    public CorsWebFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsWebFilter(source);
+    }
+    private CorsConfiguration buildConfig(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 允许跨域访问的域名
+        corsConfiguration.addAllowedOrigin("*");
+        // 请求头
+        corsConfiguration.addAllowedHeader("*");
+        // 请求方法
+        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+        corsConfiguration.addAllowedMethod(HttpMethod.POST);
+        corsConfiguration.addAllowedMethod(HttpMethod.GET);
+        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+        // 预检请求的有效期，单位为秒。
+        //corsConfiguration.setMaxAge(3600L);
+        // 是否支持安全证书
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
     }
 }
